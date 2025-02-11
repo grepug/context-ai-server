b8e72b3ce6143fdeac303ee3c214d0f65ccb8566
Kai Shao <grepug@icloud.com>
2025-02-11 16:28:02 +0800

    Update FindPhrasesCompletion to handle additional data fields and adjust parsing logic

--- Sources/ContextAIServer/Completions/FindPhrasesCompletion.swift
+++ Sources/ContextAIServer/Completions/FindPhrasesCompletion.swift
@@ -64,20 +64,25 @@ extension FindPhrasesCompletion: @retroactive AILLMCompletion {
             }
 
         let phrases = items.compactMap { item -> Output.Phrase? in
-            let parts = item.split(separator: ";;;")
+            let parts = item.split(separator: "#;;;")
                 .map { $0.trimmingCharacters(in: .whitespaces) }
 
-            guard parts.count == 4 else {
+            guard parts.count == 5 else {
+                print("parts@@ not 5", parts, parts.count)
                 return nil
             }
 
             let phrase = parts[0]
             let lemma = parts[1]
-            let adja = parts[2]
-            let sense = handleMultipleLocales(String(parts[3]))
+            let sense = handleMultipleLocales(String(parts[2]))
+            let adja = phrase
+            let syn = parts[3]
+            let desc = parts[4]
 
-            return .init(phrase: phrase, lemma: lemma, adja: adja, sense: sense)
+            return .init(phrase: phrase, lemma: lemma, adja: adja, sense: sense, desc: desc, syn: syn)
         }
+
+        assert(phrases.count == items.count, "expected \(items.count) phrases, got \(phrases.count)")
 
         return .init(phrases: phrases)
     }

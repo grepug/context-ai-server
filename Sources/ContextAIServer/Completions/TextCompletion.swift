4d81e174a09fe5e3cff6e41b090b636d7f84049d
Kai Shao <grepug@icloud.com>
2025-02-12 11:53:36 +0800

    Update dependencies in Package.resolved and remove unused TextCompletionKey extension

--- Sources/ContextAIServer/Completions/TextCompletion.swift
+++ Sources/ContextAIServer/Completions/TextCompletion.swift
@@ -1,23 +1,2 @@
 import ContextAI
 import SwiftAI
-
-extension TextCompletionKey {
-    public var template: String {
-        switch self {
-        case .collocations: PromptTemplates.collocations
-        case .translator: PromptTemplates.translator
-        case .studyNotes: PromptTemplates.studyNotes
-        case .thesaurus: PromptTemplates.thesaurus
-        case .memorizingHelper: PromptTemplates.memorizingHelper
-        case .usages: PromptTemplates.usages
-        }
-    }
-
-    public func makeCompletion(input: AICompletionNormalizedInput) -> AIStaticTextCompletion<AICompletionNormalizedInput> {
-        .init(
-            key: rawValue,
-            input: input,
-            staticTemplate: template
-        )
-    }
-}

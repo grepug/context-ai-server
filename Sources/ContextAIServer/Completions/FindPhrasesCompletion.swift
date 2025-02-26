349b3339cf7aa09c75195d3a2d452dd9828b4094
Kai Shao <grepug@icloud.com>
2025-02-26 16:24:19 +0800

    Refactor FindPhrasesCompletion to improve output handling; add condition to stop processing on specific phrase and adjust whitespace limit for lemma validation

--- Sources/ContextAIServer/Completions/FindPhrasesCompletion.swift
+++ Sources/ContextAIServer/Completions/FindPhrasesCompletion.swift
@@ -9,16 +9,30 @@ extension FindPhrasesCompletion: @retroactive AIStreamCompletion {
     public func makeOutput(chunk: String, cache: inout String) -> (output: Output?, shouldStop: Bool) {
         cache += chunk
 
-        return (makeOutput(string: cache), false)
+        if cache.contains("无符合要求的短语") {
+            return (nil, true)
+        }
+
+        if let match = cache.firstMatch(of: #/%\^(.+?)\^\^(.*?)$/#) {
+            let string = String(match.output.1)
+            let output = _makeOutput(string: string)
+            cache = String(match.output.2)
+
+            assert(output.phrases.count == 1)
+
+            return (output, false)
+        }
+
+        return (nil, false)
     }
 
     public func makeOutput(string: String) -> Output {
+        fatalError("makeOutput(string:) should not be called")
+    }
+
+    private func _makeOutput(string: String) -> Output {
         let items = string.split(separator: "\n")
-            .map {
-                $0.trimmingCharacters(in: .whitespaces)
-                    .replacingOccurrences(of: "%^", with: "")
-                    .replacingOccurrences(of: "^^", with: "")
-            }
+            .map { $0.trimmingCharacters(in: .whitespaces) }
 
         let phrases = items.compactMap { item -> Output.Phrase? in
             let parts = item.split(separator: "#;;;")
@@ -32,7 +46,7 @@ extension FindPhrasesCompletion: @retroactive AIStreamCompletion {
             let lemma = parts[1]
 
             // has 3 or less white spaces
-            guard lemma.components(separatedBy: .whitespaces).count <= 3 else {
+            guard lemma.components(separatedBy: .whitespaces).count <= 6 else {
                 print("lemma too many words", lemma)
                 return nil
             }
